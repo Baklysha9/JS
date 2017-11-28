4aa4d671869969140ba0ac97a23c54bb6abca635
Baklysha9 <bogdancoo@yandex.ru>
2017-11-28 10:14:49 +0300

    begin slider and design and logo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+var cart = {}; // моя корзина
+
 $ ('document').ready(function(){
 	loadGoods();
+	checkCart();
+	showMiniCart();
 });
 function loadGoods(){
 	//загружаю товары на страницу
@@ -12,8 +16,39 @@ function loadGoods(){
 			out+='<p>Цена: '+data[key]['cost']+'</p>';
 			out+='<img src="'+data[key].img+'">';
 			out+='<h4>'+data[key]['description']+'</h3>';
+			out+='<button class="add-to-cart" data-art="'+key+'">В корзину</button>';
 			out+='</div>';
 		}
 		$('#goods').html(out);
-	})
+		$('button.add-to-cart').on('click', addToCart);
+ 	});
 }
+ 	function addToCart(){
+ 		//добавляем товары в корзину
+ 		var articul = $(this).attr('data-art');
+ 		if (cart[ articul]!=undefined) {
+ 			cart[articul]++;
+ 		}
+ 		else {
+ 			cart[articul]=1;
+ 		}
+ 		localStorage.setItem('cart', JSON.stringify(cart));
+ 		//console.log(cart);
+ 		showMiniCart();
+ 	}
+
+ 	function checkCart(){
+ 		//проверяю наличие корзины в localstorage
+ 		if (localStorage.getItem('cart')!= null) {
+ 			cart = JSON.parse (localStorage.getItem('cart'));
+ 		}
+ 	}
+
+ 	function showMiniCart(){
+ 		//показываю содержимое корзины 
+ 		var out = '';
+ 		for (var w in cart){
+ 			out += w + '----' + cart[w]+'<br>';
+ 		}
+ 		$('#mini-cart').html(out);
+ 	}
